955f4b3275a9c2cac29134dce79423ac981aaf16
shuoya <timehet556947@gmail.com>
2020-10-22 20:05:00 +0800

    發牌

--- dice/Form1.cs
+++ dice/Form1.cs
@@ -90,5 +90,34 @@ namespace dice
         {
 
         }
+        public List<int> GetPoker(int count)
+        {
+            List<int> result = new List<int>();
+
+            try
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    result.Add(i);
+                }
+
+                for (int i = count - 2; i >= 0; i--)
+                {
+                    int lastindex = i + 1;
+                    int randomIndex = (new Random()).Next(i);
+
+                    int temp = result[lastindex];
+                    result[lastindex] = result[randomIndex];
+                    result[randomIndex] = temp;
+                }
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine(exp.ToString());
+                throw;
+            }
+
+            return result;
+        }
     }
 }
